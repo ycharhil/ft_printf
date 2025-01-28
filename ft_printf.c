a22e2ab985fbc6eb137141d783c9dfa654520f21
Yacine Charhil <ycharhil@paul-f3Br5s1.clusters.42paris.fr>
2025-01-28 13:34:20 +0100

    finish

--- ft_printf.c
+++ ft_printf.c
@@ -43,67 +43,21 @@ static size_t	conversion(const char *c, size_t *i, va_list *ap)
 int	ft_printf(const char *str, ...)
 {
 	int		len;
-	int		tmp;
-	va_list	ap;
 	size_t	i;
+	va_list	ap;
 
-	if (!str)
-		return (-1);
 	len = 0;
 	i = 0;
+	if (!str)
+		return (-1);
 	va_start(ap, str);
-	while (str[i] && (len != -1))
+	while (str[i])
 	{
 		if (str[i] == '%')
-		{
-			tmp = conversion(str + i, &i, &ap);
-			i++;
-		}
+			len += conversion(str + i++, &i, &ap);
 		else
-		{
-			tmp = write(1, &str[i], 1);
-			i++;
-		}
-		if (tmp >= 0)
-			len += tmp;
-		else
-			len = tmp;
+			len += write(1, &str[i++], 1);
 	}
 	va_end(ap);
 	return (len);
 }
-
-/*#include <stdio.h>
-
-int main() {
-    int ret_ft_printf;
-    int ret_printf;
-	int	x;
-	int y;
-	int	p;
-	int	l;
-    char c = 'A';
-    char *str = "Hello, World!";
-    int num = 12345;
-    unsigned int u_num = 98765;
-    unsigned int hex_num = 255;
-   // printf("\nTest avec plusieurs spécificateurs de format:\n");
-	ft_printf("Test avec plusieurs spécificateurs de format:\n");
-    ret_ft_printf = ft_printf("ft_printf: %c %s %d %u %x\n", c, str, num, u_num, hex_num);
-    ret_printf = printf("printf_ft: %c %s %d %u %x\n", c, str, num, u_num, hex_num);
-    printf("ret_ft_printf: %d, ret_printf: %d\n", ret_ft_printf, ret_printf);
-	//x = ft_printf("news %c \n", c);
-	//y = printf("newss %c \n", c);
-	//printf("x: %d, y: %d\n", x, y);
-	//p = ft_printf("test1: %x \n", hex_num);
-	//l = printf("test2: %x \n", hex_num);
-	//printf("p: %d l: %d\n", p, l);
-    return 0;
-}
-// voir le man de write
-// %s correct
-// %c correct
-// %d correct
-// %u correct
-// %x correct
-*/
